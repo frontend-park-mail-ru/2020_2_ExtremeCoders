d38fd6b3c2bfbf978f4d9bdf37be26ad6a27cd94
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-10-06 01:28:28 +0300

    работна над сервером на ноде, отправка запросов на сервер логики, нужно доработать передачу параметров

--- server.js
+++ server.js
@@ -1,58 +1,33 @@
-// http.createServer(function(request, response){
-//
-//     console.log("Url: " + request.url);
-//     console.log("Тип запроса: " + request.method);
-//     if(request.url==="/proxi"){
-//         response.statusCode = 302; // временная переадресация
-//         // на адрес localhost:3000/newpage
-//         response.setHeader("Location", ":8080/newpage");
-//         //response.write("METHOD "+request.method)
-//     }
-//
-//     console.log("User-Agent: " + request.headers["user-agent"]);
-//     console.log("Все заголовки");
-//     console.log(request.headers);
-//
-//
-//     console.log(`Запрошенный адрес: ${request.url}`);
-//     // получаем путь после слеша
-//     let filePath = "public/"+request.url.substr(1);
-//     if(filePath==="public/" ||filePath==="public" ){
-//         filePath="public/index.html"
-//     }
-//     // смотрим, есть ли такой файл
-//     fs.access(filePath, fs.constants.R_OK, err => {
-//         // если произошла ошибка - отправляем статусный код 404
-//         if(err){
-//             response.statusCode = 404;
-//             response.end("Resourse not found!");
-//         }
-//         else{
-//             fs.createReadStream(filePath).pipe(response);
-//         }
-//     });
-//
-// }).listen(3000);
-
 const express = require("express");
 
 const app = express();
+const req = require('request');
+fetch = require('node-fetch')
+
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(request, response){
+app.get("/", function (request, response) {
     response.redirect("public/index.html")
-    console.log(request.url)
+    console.log("Get:", request.url)
 
-   // response.send(request.url)
+    // response.send(request.url)
 });
 
-
-app.post(/.*$/, function(request, response){
-    console.log("POST", console.log(request.url))
-    //response.send("POST " + request.url)
-    response.redirect("http://localhost:8080/")
-
+app.post(/.*$/, (request, response) => {
+    req.post( {
+            url:'http://localhost:8080' + request.url,
+            form: {
+                login: 'login1',
+                password: 'password1',
+            }
+        }
+        , (err, resp, body) => {
+            console.log(request.body)
+            if (err)
+                return response.status(500).send({message: err})
+            response.send(body)
+        })
 });
 
 app.listen(3000);
