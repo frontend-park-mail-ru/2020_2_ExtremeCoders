501d4de07deb6bcf2c67f2722963c635205b67e6
Dellvin <dellvin.black@gmail.com>
2020-10-06 19:36:52 +0300

    do smth

--- public/login.js
+++ public/login.js
@@ -11,13 +11,13 @@ class SignIn {
     const form = document.createElement('form');
 
     const title = createText('h1', 'Вход', 'signup_title');
-    const login = createInput('email', 'Введите логин', 'login');
+    const email = createInput('email', 'Введите логин', 'email');
     const password1 = createInput('password', 'Введите пароль', 'password1');
 
     const confirmButton = createButton('submit', 'Войти', 'confirmSignupButton');
     const backButton = createHref('tmp-form_button', 'Регистрация', 'signup');
     form.appendChild(title);
-    form.appendChild(login);
+    form.appendChild(email);
     form.appendChild(password1);
     form.appendChild(confirmButton);
     form.appendChild(backButton)
