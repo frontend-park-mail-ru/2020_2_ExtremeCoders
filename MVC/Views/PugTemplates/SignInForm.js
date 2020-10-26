77ec9bec29700855910bc1854a5ac05edf9a243d
Dellvin <dellvin.black@gmail.com>
2020-10-26 23:36:30 +0300

    fix pug

--- MVC/Views/PugTemplates/SignInForm.js
+++ MVC/Views/PugTemplates/SignInForm.js
@@ -70,31 +70,31 @@ function signinformTemplate(locals) {
     var pug_debug_filename, pug_debug_line;
     try {
         var pug_debug_sources = {
-            "SignInForm.pug": "include  BaseComponents\u002Fbutton\ninclude BaseComponents\u002FpageTitle\ninclude BaseComponents\u002FText\ninclude BaseComponents\u002Favatar\ninclude BaseComponents\u002FclassNames\n\n+pageTitle('Войти')\nform(method='POST' class=classNames.formClass)\n    +text('Login')\n    input(type='text', name='email')\n    +text('Password')\n    input(type='password', name='password' class=classNames.inputClass)\n    input(type='submit', value='Войти', class=classNames.inputClass)\n    +button('Регистрация','signup')\n    +button('Назад','back')\n\n\u002F\u002Fexport {signinformTemplate}",
-            "BaseComponents\u002Fbutton.pug": "mixin button(text, name)\n    button(class='', name=name) #{text}",
-            "BaseComponents\u002FpageTitle.pug": "mixin pageTitle(text)\n    h1(class='') #{text}",
-            "BaseComponents\u002FText.pug": "mixin text(text, name)\n    p(class='', name=name) #{text}",
+            "SignInForm.pug": "include  BaseComponents\u002Fbutton\ninclude BaseComponents\u002FpageTitle\ninclude BaseComponents\u002FText\ninclude BaseComponents\u002Favatar\ninclude BaseComponents\u002FclassNames\n\n+pageTitle('Войти')\nform(method='POST' class=classNames.formClass)\n    +text('Login')\n    input(type='text', name='email', class=classNames.inputClass)\n    +text('Password')\n    input(type='password', name='password', class=classNames.inputClass)\n    input(type='submit', value='Войти', class=classNames.inputClass)\n    +button('Регистрация','signup', classNames.loginButton)\n    +button('Назад','back', classNames.regButton)\n\n\u002F\u002Fexport {signinformTemplate}",
+            "BaseComponents\u002Fbutton.pug": "mixin button(text, name, className)\n    button(class=className, name=name) #{text}",
+            "BaseComponents\u002FpageTitle.pug": "mixin pageTitle(text, className)\n    h1(class=className) #{text}",
+            "BaseComponents\u002FText.pug": "mixin text(text, name)\n    p(class='MainTitle', name=name) #{text}",
             "BaseComponents\u002Favatar.pug": "mixin avatar(src)\n    img(width=70, height=70, id='avatar', src=src)",
-            "BaseComponents\u002FclassNames.pug": "- var classNames= { inputClass:'input', formClass:'form' }"
+            "BaseComponents\u002FclassNames.pug": "- var classNames ={inputClass:'login-reg-input',formClass:'login-form',regButton:'reg-button',loginButton:'login-button'}"
         };
         ;pug_debug_line = 1;
         pug_debug_filename = "BaseComponents\u002Fbutton.pug";
-        pug_mixins["button"] = pug_interp = function (text, name) {
+        pug_mixins["button"] = pug_interp = function (text, name, className) {
             var block = (this && this.block), attributes = (this && this.attributes) || {};
             ;pug_debug_line = 2;
             pug_debug_filename = "BaseComponents\u002Fbutton.pug";
-            pug_html = pug_html + "\u003Cbutton" + ("" + pug_attr("name", name, true, false)) + "\u003E";
+            pug_html = pug_html + "\u003Cbutton" + (pug_attr("class", pug_classes([className], [true]), false, false) + pug_attr("name", name, true, false)) + "\u003E";
             ;pug_debug_line = 2;
             pug_debug_filename = "BaseComponents\u002Fbutton.pug";
             pug_html = pug_html + (pug_escape(null == (pug_interp = text) ? "" : pug_interp)) + "\u003C\u002Fbutton\u003E";
         };
         ;pug_debug_line = 1;
         pug_debug_filename = "BaseComponents\u002FpageTitle.pug";
-        pug_mixins["pageTitle"] = pug_interp = function (text) {
+        pug_mixins["pageTitle"] = pug_interp = function (text, className) {
             var block = (this && this.block), attributes = (this && this.attributes) || {};
             ;pug_debug_line = 2;
             pug_debug_filename = "BaseComponents\u002FpageTitle.pug";
-            pug_html = pug_html + "\u003Ch1\u003E";
+            pug_html = pug_html + "\u003Ch1" + (pug_attr("class", pug_classes([className], [true]), false, false)) + "\u003E";
             ;pug_debug_line = 2;
             pug_debug_filename = "BaseComponents\u002FpageTitle.pug";
             pug_html = pug_html + (pug_escape(null == (pug_interp = text) ? "" : pug_interp)) + "\u003C\u002Fh1\u003E";
@@ -105,7 +105,7 @@ function signinformTemplate(locals) {
             var block = (this && this.block), attributes = (this && this.attributes) || {};
             ;pug_debug_line = 2;
             pug_debug_filename = "BaseComponents\u002FText.pug";
-            pug_html = pug_html + "\u003Cp" + ("" + pug_attr("name", name, true, false)) + "\u003E";
+            pug_html = pug_html + "\u003Cp" + (" class=\"MainTitle\"" + pug_attr("name", name, true, false)) + "\u003E";
             ;pug_debug_line = 2;
             pug_debug_filename = "BaseComponents\u002FText.pug";
             pug_html = pug_html + (pug_escape(null == (pug_interp = text) ? "" : pug_interp)) + "\u003C\u002Fp\u003E";
@@ -116,7 +116,12 @@ function signinformTemplate(locals) {
 
         ;pug_debug_line = 1;
         pug_debug_filename = "BaseComponents\u002FclassNames.pug";
-        var classNames = {inputClass: 'input', formClass: 'form'}
+        var classNames = {
+                inputClass: 'login-reg-input',
+                formClass: 'login-form',
+                regButton: 'reg-button',
+                loginButton: 'login-button'
+            }
         ;pug_debug_line = 7;
         pug_debug_filename = "SignInForm.pug";
         pug_mixins["pageTitle"]('Войти');
@@ -128,7 +133,7 @@ function signinformTemplate(locals) {
         pug_mixins["text"]('Login');
         ;pug_debug_line = 10;
         pug_debug_filename = "SignInForm.pug";
-        pug_html = pug_html + "\u003Cinput type=\"text\" name=\"email\"\u002F\u003E";
+        pug_html = pug_html + "\u003Cinput" + (pug_attr("class", pug_classes([classNames.inputClass], [true]), false, false) + " type=\"text\" name=\"email\"") + "\u002F\u003E";
         ;pug_debug_line = 11;
         pug_debug_filename = "SignInForm.pug";
         pug_mixins["text"]('Password');
@@ -140,10 +145,10 @@ function signinformTemplate(locals) {
         pug_html = pug_html + "\u003Cinput" + (pug_attr("class", pug_classes([classNames.inputClass], [true]), false, false) + " type=\"submit\" value=\"Войти\"") + "\u002F\u003E";
         ;pug_debug_line = 14;
         pug_debug_filename = "SignInForm.pug";
-        pug_mixins["button"]('Регистрация', 'signup');
+        pug_mixins["button"]('Регистрация', 'signup', classNames.loginButton);
         ;pug_debug_line = 15;
         pug_debug_filename = "SignInForm.pug";
-        pug_mixins["button"]('Назад', 'back');
+        pug_mixins["button"]('Назад', 'back', classNames.regButton);
         pug_html = pug_html + "\u003C\u002Fform\u003E";
         ;pug_debug_line = 17;
         pug_debug_filename = "SignInForm.pug";
