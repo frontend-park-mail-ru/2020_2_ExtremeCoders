2f8f38d9ad71d61176d4cd264461e47845b499f5
Dellvin <dellvin.black@gmail.com>
2020-11-09 18:29:25 +0300

    trying to fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 module.exports = {
-    entry: './src/main.js',
+    entry: [
+        './src/main.js',
+        './src/Views/public/css/main.css'
+    ],
     module:{
         rules:[
             {test: /\.svg$/, use: 'svg-inline-loader'},
@@ -15,12 +19,17 @@ module.exports = {
                     }
                 }
             }
-        ]
+        ],
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            filename: './src/index.html'
+        })
+    ],
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 }
-
