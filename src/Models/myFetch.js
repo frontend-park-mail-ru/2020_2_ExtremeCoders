370a8a85cb2d4d52881d2d2316195291dce7d9f5
marksadykov <sadykov.mark@gmail.com>
2020-11-14 11:45:05 +0300

    fix-fetch

--- src/Models/myFetch.js
+++ src/Models/myFetch.js
@@ -1,12 +1,18 @@
 import { Paths } from '../Constants.js';
 
 export default function myFetch(url, method, body) {
+  let token = String(document.cookie.match(/token=([\w-]+)/)[0]);
+  console.log(token);
+  token = token.replace('token=', '');
+  console.log(token);
   return fetch(Paths.baseUrl + url,
     {
       method,
       mode: 'cors',
       credentials: 'include',
-      csrf_token: document.cookie.match(/CSRF-TOKEN=([\w-]+)/),
+      headers: {
+        csrf_token: token,
+      },
       body,
     });
 }
