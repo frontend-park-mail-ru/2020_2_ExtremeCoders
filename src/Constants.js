2f0aa988054ee5bfa2f59ced06f1fdf64816d65e
marksadykov <sadykov.mark@gmail.com>
2020-11-15 17:52:44 +0300

    fix-fetch

--- src/Constants.js
+++ src/Constants.js
@@ -77,7 +77,7 @@ const Events = {
 };
 
 const Paths = {
-  baseUrl: 'http://localhost:1323',
+  baseUrl: 'http://localhost:8080',
   mainPage: '/letters',
   signInPage: '/signin',
   signUpPage: '/signup',
