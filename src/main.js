7fce885d5ee6eb16052299f6c59c6d81d6142326
marksadykov <sadykov.mark@gmail.com>
2020-12-21 18:15:54 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -41,8 +41,8 @@ const profileEditView = new ProfileEditView(document.body);
 const mainPageView = new MainPageView(document.body);
 const sendLetterView = new SendLetterView(document.body);
 
-userModel.setUrl('http://localhost:8080');
-const letterModel = new LetterModel('http://localhost:8080');
+userModel.setUrl('http://95.163.209.195:8080');
+const letterModel = new LetterModel('http://95.163.209.195:8080');
 
 mainPageController.setView(mainPageView);
 
